ed01754f555974cfd4254457b8bd25124e4623a5
Amrutha Gujjar <gujjaramrutha@gmail.com>
2022-10-19 18:27:28 -0400

    Update button.js

--- button.js
+++ button.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('button', {
         button = document.createElement('a-entity');
         button.setAttribute('class', 'button');
         button.setAttribute('text',"value: START; width: 2; color: #000000; font: mozillavr; align: center;");
-        button.setAttribute('position','2 2 0');
+        button.setAttribute('position','0.75 1.5 0');
         button.setAttribute('material','color: blue');
         button.setAttribute('geometry','primitive: plane; width: 0.5; height: 0.25');
         button.setAttribute('clickable','');
@@ -15,11 +15,18 @@ AFRAME.registerComponent('button', {
         
         // fruit[i].setAttribute('aabb-collider', 'objects: .monkey');
 
-        sceneEl.addEventListener('onclick', 
+        sceneEl.addEventListener('grab-start', 
             function () {
                 let sky = document.querySelector('a-sky');
                 sky.setAttribute('color','#FF0000');
             }
         );
+
+        sceneEl.addEventListener('grab-end', 
+        function () {
+            let sky = document.querySelector('a-sky');
+            sky.setAttribute('color','#6796f5');
+        }
+    );
     }
 });
